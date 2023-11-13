3913c2b2308a0695ec24276dd6be8f420285a55a
jasmineLoso <jasmil4@uw.edu>
2023-11-13 11:13:42 -0800

    some work for the components

--- project-draft/components/App.js
+++ project-draft/components/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import { ReactDOM } from 'react-dom/client';
+
+//import css:
+import 'bootstrap/dist/css/bootstrap.css';
+import './style.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-
+        <App />
     </React.StrictMode>
 );
---
+++ project-draft/components/indexRendering.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { App } from './components.App';
+
+export function App(props) {
+    return (
+        <div>
+            {/*call components here*/}
+        </div>
+    );
+}
