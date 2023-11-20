a4966c2ab2ca31f8b47f6e1ff8637b5266bfd8d2
jasmineLoso <jasmil4@uw.edu>
2023-11-20 09:12:11 -0800

    all components are now called in app

--- project-draft-2/src/components/App.js
+++ project-draft-2/src/components/App.js
@@ -3,14 +3,18 @@ import { ReactDOM } from 'react-dom/client';
 
 //import your components here:
 import { Homepage } from './Homepage.js';
+import { Aboutus } from './Aboutus.js';
 import { Feed } from './Feed.js';
+import { userProfile } from './userProfile.js';
 import { Form } from './Form.js';
 
 export default function App() {
   //call your components here:
   return (
-   //  <Homepage />
-     <Feed />
+    <Homepage />
+    // <Aboutus />
+    // <Feed />
+    // <userProfile />
     // <Form />
   );
 }
--- project-draft-2/src/components/indexRendering.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import App from './components.App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
-
