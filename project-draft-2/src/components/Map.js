0b4f88bdc10f169105c353cf0d1c9ee7d6654c19
jessica <jwang717@uw.edu>
2023-12-04 00:03:04 -0800

    Add markers to map

--- project-draft-2/src/components/Map.js
+++ project-draft-2/src/components/Map.js
@@ -1,21 +1,30 @@
 import React from 'react';
 import '../style.css';
+import 'leaflet-markers';
 
 export function Map() {
+    let map = document.createElement('iframe');
+        map.src = "https://snazzymaps.com/embed/547777";
+        map.width = "75%";
+        map.height = "600px";
+        map.style.border = 'none';
+
+    const marker = L.icon {
+        iconUrl: '../../project-draft-2/public/img/favicon.ico';
+        iconSize: [32, 32];
+        iconAnchor: [16, 32];
+        popupAnchor: [0, -32];
+    }
+
     map.addListener('click', function(event) {
-        addMarker(event.latLng);
-      });
+        L.marker([event.latlng], { icon: marker }, addTo(map));
+    });
 
     return (
         <main>
             {/* <section id="map">  */}
                 <div id="map">
-                    <iframe
-                        src="https://snazzymaps.com/embed/547777"
-                        width="75%"
-                        height="600px"
-                        style={{ border: 'none' }}
-                    ></iframe>
+
                 </div>
             {/* </section> */}
         </main>
