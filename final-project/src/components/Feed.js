2a085eebb8dedfbb6807c7904931725ba74747c9
kylahmoon <kylahm@uw.edu>
2023-12-06 13:00:22 -0800

    Saved Posts Updates

--- final-project/src/components/Feed.js
+++ final-project/src/components/Feed.js
@@ -4,10 +4,10 @@ import { NavLink } from 'react-router-dom';
 
 export default function Feed(props) {
     const data = props.data;
-    const [savedPosts, setSavedPosts] = useState([]);
+    const [savedMusic, setSavedMusic] = useState([]);
 
     const handleSaveClick = (post) => {
-        setSavedPosts([...savedPosts, post]);
+        setSavedMusic([...savedMusic, post]);
     };
 
     const samplePost = data.map((userData) => (
@@ -46,7 +46,7 @@ export default function Feed(props) {
 
             {/* Save button */}
             <div className="save">
-                <button type="button" className="btn btn-info"  onClick={() => handleSaveClick(samplePost)}>
+                <button type="button" className="btn btn-info"  onClick={() => handleSaveClick(userData)}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-bookmark-heart" viewBox="0 0 16 16">
                         <path fillRule="evenodd" d="M8 4.41c1.387-1.425 4.854 1.07 0 4.277C3.146 5.48 6.613 2.986 8 4.412z"></path>
                         <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5V2zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1H4z"></path>
--- final-project/src/components/SavedMusic.js
+++ final-project/src/components/SavedMusic.js
@@ -1,26 +1,29 @@
 import React from 'react';
 
-export default function SavedPosts(props) {
+export default function SavedMusic(props) {
     const savedPosts = props.data;
 
     return (
-        <div className="savedPost">
-            <h2 className="sub-header">Saved Songs</h2>
-            <div className="saved-songs">
-                {savedPosts.map((post) => (
-                    <div key={post.ID} className="col-12 col-md-4">
-                        <div className="card">
-                            <div className="card-body">
-                                <h2 className="song-name">{post.songTitle}</h2>
-                                <h3 id="artist">{post.songArtist}</h3>
-                                <button className="genre">{post.genre}</button>
-                                <button className="location">{post.Location}</button>
-                                <button className="activity">{post.activity}</button>
+        <div className="feed">
+            <main>
+                <h1 className="musicmatch-header">MUSICMATCH</h1>
+                <h2 className="sub-header">Saved Songs</h2>
+                <div className="saved-songs">
+                    {savedPosts.map((post) => (
+                        <div key={post.ID} className="col-12 col-md-4">
+                            <div className="card">
+                                <div className="card-body">
+                                    <h2 className="song-name">{post.songTitle}</h2>
+                                    <h3 id="artist">{post.songArtist}</h3>
+                                    <button className="genre">{post.genre}</button>
+                                    <button className="location">{post.Location}</button>
+                                    <button className="activity">{post.activity}</button>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                ))}
-            </div>
+                    ))}
+                </div>
+            </main>
         </div>
     );
 }
