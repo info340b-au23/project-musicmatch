3b14ef76b5b5a9d64061d28cd837179f52e58271
jasmineLoso <jasmil4@uw.edu>
2023-12-06 09:18:14 -0800

    map work

--- final-project/src/components/Map.js
+++ final-project/src/components/Map.js
@@ -1,17 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../style.css';
 
+// export function Map() {
+//     var map = document.createElement('iframe');
+//         map.src="https://snazzymaps.com/embed/551698" 
+//         map.width="75%" 
+//         map.height="600px" 
+//         map.style="border:none;"
+//     return (
+//         <main>
+//             <div id="map">
+//                 map; 
+//             </div>
+//         </main>
+//     );
+// }
+
+
 export function Map() {
-    var map = document.createElement('iframe');
-        map.src="https://snazzymaps.com/embed/551698" 
-        map.width="75%" 
-        map.height="600px" 
-        map.style="border:none;"
+    const [map, setMap] = useState('');
+
+    useEffect(() => {
+        const iframe = document.createElement('iframe');
+        iframe.src = "https://snazzymaps.com/embed/551698";
+        iframe.width = "75%";
+        iframe.height = "600px";
+        iframe.style.border = "none";
+
+        // Append the iframe content as a string to the map state
+        setMap(iframe.outerHTML);
+
+        // Clean up the iframe when the component unmounts
+        return () => {
+            if (iframe.parentNode) {
+                iframe.parentNode.removeChild(iframe);
+            }
+        };
+    }, []);
+
     return (
         <main>
-            <div id="map">
-                map;
-            </div>
+            <div id="map" dangerouslySetInnerHTML={{ __html: map }} />
         </main>
     );
 }
