52f736e6b8a06a25eed22f5cf4dbc15dfd29dacf
kylahmoon <kylahm@uw.edu>
2023-12-09 17:22:51 -0800

    saved music updates

--- final-project/src/components/SavedMusic.js
+++ final-project/src/components/SavedMusic.js
@@ -1,30 +1,37 @@
 import React from 'react';
 
-export default function SavedMusic(props) {
-    const savedPosts = props.data
-    .map((userData) => (
-        <div key={userData.id}>
-            <div className="row">
-                {/* song name and artist */}
-                <div className="col-12 filter-info text-center">
-                    <span className="filter-item">
-                        Song name: {userData.songTitle}
-                    </span>
-                    <span className="filter-item">
-                        Artist: {userData.songArtist}
-                    </span>
-                </div>
+export default function SavedMusic() {
+    // const savedPosts = props.data
+    // .map((userData) => (
+    //     <div key={userData.id}>
+    //         <div className="row">
+    //             {/* song name and artist */}
+    //             <div className="col-12 filter-info text-center">
+    //                 <span className="filter-item">
+    //                     Song name: {userData.songTitle}
+    //                 </span>
+    //                 <span className="filter-item">
+    //                     Artist: {userData.songArtist}
+    //                 </span>
+    //             </div>
                 
-            </div>
+    //         </div>
 
-        </div>
-    ));
+    //     </div>
+    // ));
 
-    return (
-        <div className="feed">
-            <p></p>
+    // return (
+    //     <div className="feed">
+    //         <p></p>
+    //     </div>
+    // );
+
+    return(
+        <div>
+            <p>Hello</p>
         </div>
     );
+
 }
 
 {/* <main>
