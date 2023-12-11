79365d0806be9fdd0bcf810f3cfc537e850f71f0
jessica <jwang717@uw.edu>
2023-12-11 12:51:52 -0800

    Begin user auth

--- project-draft-2/src/components/Homepage.js
+++ project-draft-2/src/components/Homepage.js
@@ -24,7 +24,7 @@ export default function Homepage() {
                 </header>
             </main>
 
-            <Map />;
+            {/* <Map />; */}
 
             {/* Footer */}
             {/* <footer>
